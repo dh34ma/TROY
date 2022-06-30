5a8ab717e412f00eccb91ae1c1a294c7f7e6a101
dh34ma <100998077+dh34ma@users.noreply.github.com>
2022-06-30 12:28:07 +0100

    Update TROY.py

--- TROY.py
+++ TROY.py
@@ -25,6 +25,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("TROY").login()
+		__import__("TROY").File()
 	else:
-		__import__("TROY").login()
+		__import__("TROY").File()
