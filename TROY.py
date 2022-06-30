2d1a8f60ce9580796da7888cd9f7860db82cfc05
dh34ma <100998077+dh34ma@users.noreply.github.com>
2022-06-30 12:35:40 +0100

    Update TROY.py

--- TROY.py
+++ TROY.py
@@ -1,2 +1,12 @@
-import xoshnaw
-TROY()
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    
+    TROY ()
